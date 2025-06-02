fa45037f7aca1bdaa4f8b85a505b4fcdf7002e1a
MeticulousTheGreat <68789903+MeticulousTheGreat@users.noreply.github.com>
2025-06-02 16:23:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -192,7 +192,7 @@ window.addEventListener("DOMContentLoaded", () => {
     
   function prepAudio () {
     //ABCJS.midi.stopPlaying();
-    const tempo = pareseInt(document.getElementById("tempoSlider").value);
+    const tempo = parseInt(document.getElementById("tempoSlider").value);
     convertedABC = `Q:1/4=${tempo} \n` + notesToABC(notes, key, numMeasures)
     document.getElementById("rawNotation").innerHTML = convertedABC;
 
